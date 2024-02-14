6383c1a96a447442d86f1b9214b8ab6b4b380cf8
Roopam123 <roopamr2411@gmail.com>
2024-02-14 19:51:34 +0530

    Complete Bootsrap Project

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import CountUp from 'react-countup';
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
-      <div style={{fontSize:"45px"}}>
-        <CountUp start={0} end={100} duration={1}/>
-      </div>
+      <h1 className='text-primary m-5 p-1'>App</h1>
     </div>
   )
 }
